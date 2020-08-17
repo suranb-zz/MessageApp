a242fdfcb91a3fbb3c6163aca4f219d93e60ab30
Suran Basharati <suranbasharati@gmail.com>
2020-08-17 14:18:01 +0200

    Added the design layout for the chat screen

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_chat_ui_starter/models/message_model.dart';
 import 'package:flutter_chat_ui_starter/models/user_model.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -11,8 +12,83 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+
+
+  _buildMessage(Message message, bool isMe){
+    return Container(
+      margin: isMe ? EdgeInsets.only(
+        top: 8.0,
+        bottom: 8.0,
+        left: 8.0,
+      ) : EdgeInsets.only(top: 8.0, bottom: 8.0, right: 80.0),
+      padding: EdgeInsets.symmetric(horizontal: 25.0, vertical: 15.0),
+      decoration: BoxDecoration(
+        color: isMe ? Theme.of(context).accentColor : Color(0xFFfab1a0),
+        borderRadius: isMe ? BorderRadius.only(
+            topLeft: Radius.circular(15.0),
+            bottomLeft: Radius.circular(15.0),
+        ) : BorderRadius.only(
+        topRight: Radius.circular(15.0),
+        bottomRight: Radius.circular(15.0),
+        ),
+      ),
+      child: Text(message.text),
+    );
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      appBar: AppBar(
+        title: Center(
+          child: Text(widget.user.name,
+            style: TextStyle(
+              fontSize: 28.0,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+        elevation: 0.0,
+        actions: <Widget> [
+          IconButton(icon: Icon(Icons.more_horiz),
+            iconSize: 30.0,
+            color: Colors.white,
+            onPressed: ()  {
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+                child: ListView.builder(
+                  padding: EdgeInsets.only(top: 15.0),
+                itemCount: messages.length,
+                itemBuilder: (BuildContext context, int index){
+                    final Message message = messages[index];
+                    final bool isMe = message.sender.id == currentUser.id;
+                    return _buildMessage(message, isMe);
+                },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
