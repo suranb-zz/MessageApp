401cd1c5efab4561e3760f48c8a9a85063f70a9e
Suran Basharati <suranbasharati@gmail.com>
2020-08-17 12:13:10 +0200

    Added date and implemented clipping when scrolling

--- lib/widgets/recent_chats.dart
+++ lib/widgets/recent_chats.dart
@@ -14,42 +14,68 @@ class RecentChats extends StatelessWidget {
               topRight: Radius.circular(30.0),
                 ),
               ),
-              child: ListView.builder(
-              itemCount: chats.length,
-              itemBuilder: (BuildContext context, int index) {
-                final Message chat = chats[index];
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 35.0,
-                          backgroundImage: AssetImage(chat.sender.imageUrl),
-                        ),
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                            chat.sender.name,
-                        ),
-                            Text(
-                              chat.text,
-                        ),
-                      ],
-                    ),
-                  ],
+              child: ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
                 ),
-                Column(
-                  children: <Widget>[
-                    Text(chat.time),
-                    Text('NEW'),
-                  ],
-                ),
-              ],
+                child: ListView.builder(
+                itemCount: chats.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final Message chat = chats[index];
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 35.0,
+                            backgroundImage: AssetImage(chat.sender.imageUrl),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                              chat.sender.name,
+                                style: TextStyle(
+                                  color: Colors.grey,
+                                  fontSize: 15.0,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                          ),
+                              SizedBox(
+                                height: 5.0,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width * 0.45,
+                                child: Text(
+                                  chat.text,
+                                  style: TextStyle(
+                                    color: Colors.blueGrey,
+                                    fontSize: 15.0,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                  overflow: TextOverflow.ellipsis,
+                          ),
+                              ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Text(chat.time),
+                      Text('NEW'),
+                    ],
+                  ),
+                ],
             );
           },
         ),
+              ),
       ),
     );
   }
